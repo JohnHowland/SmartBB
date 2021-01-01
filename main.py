a2b5464c0a0a5a35ed2321443e22675c4b5a045d
John Howland <>
2020-12-31 22:31:35 -0500

    debug

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ try:
     print("Write ABCD to 0x0011")
     time.sleep(2.0)
     print("Reading characteristics")
-    print(device.discover_characteristics)
+    print(device.discover_characteristics())
     time.sleep(2.0)
     value = device.char_read_handle(0x000e)
     print("Value = " + str(value))
