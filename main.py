9f3a66a7db922c16b444c8b7ea2ab192c0fc81b5
John Howland <>
2020-12-31 22:05:43 -0500

    debug

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygatt
+import time
 
 
 # Bluetooth stuff
@@ -10,8 +11,12 @@ try:
     print("in Try")
     adapter.start()
     device = adapter.connect(MAC)
-    value = device.char_read("a1e8f5b1-696b-4e4c-87c6-69dfe0b0093b")
+    device.char_write_handle(0x0011, 0xABCD)
+    print("Write ABCD to 0x0011")
+    time.sleep(2.0)
+    value = device.char_read_handle(0x000e)
     print("Value = " + str(value))
+    time.sleep(2.0)
 finally:
     adapter.stop()
     print("In finally")
